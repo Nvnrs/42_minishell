3cb0273ba1ec3fb052c5b3c3a8abfa994fe2733d
Nvnrs <nicolasvenerosy@gmail.com>
2025-03-07 11:10:44 +0100

    fix leaks echo (the test : echo '   ' -n | cat -e

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: nveneros <nveneros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 11:41:30 by nveneros          #+#    #+#             */
-/*   Updated: 2025/03/06 17:15:43 by nveneros         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:09:14 by nveneros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,8 @@ void	builtin_echo(char **args)
 	}
 	while (args[i] && args[i + 1])
 	{
-		output_str = ft_strjoin(output_str, " ");
-		output_str = ft_strjoin(output_str, args[i + 1]);
+		output_str = cft_strcat_realloc(output_str, " ");
+		output_str = cft_strcat_realloc(output_str, args[i + 1]);
 		i++;
 	}
 	if ((args[0] && is_n_option_echo(args[0])))

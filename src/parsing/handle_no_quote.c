35eaeb37108c50c599bae1ab44ee2ce391d7ae43
zeradiias <132435888+zeradiias@users.noreply.github.com>
2025-03-10 09:55:55 +0100

    fixed when there was quote in no quote handling -> e'cho test'

--- src/parsing/handle_no_quote.c
+++ src/parsing/handle_no_quote.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_no_quote.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nveneros <nveneros@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pchateau <pchateau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:46:01 by nveneros          #+#    #+#             */
-/*   Updated: 2025/03/05 14:34:40 by nveneros         ###   ########.fr       */
+/*   Updated: 2025/03/10 09:54:24 by pchateau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,21 @@ int	handle_no_quote(char **split, int split_index, char *str, int i_start)
 	i_end = i_start;
 	while (str[i_end] != ' '
 		&& str[i_end] != '\0' && !is_single_operator(str, i_end))
+	{
+		if (str[i_end] == SINGLE_QUOTE)
+		{
+			i_end++;
+			while (str[i_end] != SINGLE_QUOTE)
+				i_end++;
+		}
+		else if (str[i_end] == DOUBLE_QUOTE)
+		{
+			i_end++;
+			while (str[i_end] != DOUBLE_QUOTE)
+				i_end++;
+		}
 		i_end++;
+	}
 	split[split_index] = ft_substr(str, i_start, i_end - i_start);
 	return (i_end - i_start);
 }
@@ -31,6 +45,20 @@ int	count_no_quote(char *str, int i_start)
 	i_end = i_start;
 	while (str[i_end] != ' '
 		&& str[i_end] != '\0' && !is_single_operator(str, i_end))
+	{
+		if (str[i_end] == SINGLE_QUOTE)
+		{
+			i_end++;
+			while (str[i_end] != SINGLE_QUOTE)
+				i_end++;
+		}
+		else if (str[i_end] == DOUBLE_QUOTE)
+		{
+			i_end++;
+			while (str[i_end] != DOUBLE_QUOTE)
+				i_end++;
+		}
 		i_end++;
+	}
 	return (i_end - i_start);
 }

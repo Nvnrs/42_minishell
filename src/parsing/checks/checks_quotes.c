552465908eaee0884fc676508c6699f6326ab3d9
Nvnrs <nicolasvenerosy@gmail.com>
2025-03-10 11:03:29 +0100

    fix invalid read nb_quotes_is_even

--- src/parsing/checks/checks_quotes.c
+++ src/parsing/checks/checks_quotes.c
@@ -6,19 +6,33 @@
 /*   By: nveneros <nveneros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:52:28 by nveneros          #+#    #+#             */
-/*   Updated: 2025/03/07 17:38:44 by nveneros         ###   ########.fr       */
+/*   Updated: 2025/03/10 10:58:24 by nveneros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	skip_inside_quote(char quote, char *str, int *i)
+// return nb_quotes : open/close -> 2 | only open 1 
+int	skip_inside_quote(char quote, char *str, int *i)
 {
+	t_bool	quote_open;
+	t_bool	quote_close;	
+
 	*i += 1;
+	quote_open = TRUE;
+	quote_close = FALSE;
 	while (str[*i] && str[*i] != quote)
 	{
 		*i += 1;
 	}
+	if (str[*i] == quote)
+	{
+		quote_close = TRUE;
+		*i+= 1;	
+	}
+	if (quote_open && quote_close)
+		return (2);
+	return (1);
 }
 
 t_bool	nb_quotes_is_even(char quote, char other_quote, char *str)
@@ -32,15 +46,12 @@ t_bool	nb_quotes_is_even(char quote, char other_quote, char *str)
 	{
 		if (str[i] == other_quote)
 			skip_inside_quote(other_quote, str, &i);
-		else if (str[i] == quote)
+		else if (str[i] == quote) 
 		{
-			skip_inside_quote(quote, str, &i);
-			if (str[i] == quote)
-				count += 2;
-			else
-				count++;
+			count += skip_inside_quote(quote, str, &i);
 		}
-		i++;
+		else
+			i++;
 	}
 	return (count % 2 == 0);
 }

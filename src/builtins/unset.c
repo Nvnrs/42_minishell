bc12715f166c8b7159ff118962c26a0f4615408a
Nvnrs <nicolasvenerosy@gmail.com>
2025-03-11 11:19:22 +0100

    norminette unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,11 +6,27 @@
 /*   By: nveneros <nveneros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:49:14 by nveneros          #+#    #+#             */
-/*   Updated: 2025/03/06 17:18:12 by nveneros         ###   ########.fr       */
+/*   Updated: 2025/03/11 11:16:38 by nveneros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	key_is_in_env(t_list **curr, t_list *last, t_list **env)
+{
+	
+	if (last != NULL)
+	{
+		last->next = (*curr)->next;
+		ft_lstdelone(*curr, free_key_val);
+		*curr = last->next;
+	}
+	else
+	{
+		*env = (*curr)->next;
+		ft_lstdelone(*curr, free_key_val);
+	}
+}
 
 /*UNSET */
 void	remove_var(char *var_key, t_list **env)
@@ -26,17 +42,7 @@ void	remove_var(char *var_key, t_list **env)
 		content_env = curr->content;
 		if (ft_strcmp(content_env->key, var_key) == 0)
 		{
-			if (last != NULL)
-			{
-				last->next = curr->next;
-				ft_lstdelone(curr, free_key_val);
-				curr = last->next;
-			}
-			else
-			{
-				*env = curr->next;
-				ft_lstdelone(curr, free_key_val);
-			}
+			key_is_in_env(&curr, last, env);
 			return ;
 		}
 		last = curr;
